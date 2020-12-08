730401f410a503d2b8e0a73bb7229ce63ec60e89
shu <60308179+booksuper@users.noreply.github.com>
2020-12-08 21:06:46 +0800

    Add files via upload

---
+++ facerecognize/facerecognize/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace facerecognize
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.opencap = new System.Windows.Forms.Button();
+            this.savebtn = new System.Windows.Forms.Button();
+            this.trainbtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labeltextBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(12, 23);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(555, 313);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            this.imageBox1.Click += new System.EventHandler(this.imageBox1_Click);
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(623, 23);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(115, 110);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // opencap
+            // 
+            this.opencap.Location = new System.Drawing.Point(193, 351);
+            this.opencap.Name = "opencap";
+            this.opencap.Size = new System.Drawing.Size(75, 23);
+            this.opencap.TabIndex = 3;
+            this.opencap.Text = "打开摄像头";
+            this.opencap.UseVisualStyleBackColor = true;
+            this.opencap.Click += new System.EventHandler(this.opencap_Click);
+            // 
+            // savebtn
+            // 
+            this.savebtn.Location = new System.Drawing.Point(417, 351);
+            this.savebtn.Name = "savebtn";
+            this.savebtn.Size = new System.Drawing.Size(75, 23);
+            this.savebtn.TabIndex = 4;
+            this.savebtn.Text = "保存图片";
+            this.savebtn.UseVisualStyleBackColor = true;
+            this.savebtn.Click += new System.EventHandler(this.savebtn_Click);
+            // 
+            // trainbtn
+            // 
+            this.trainbtn.Location = new System.Drawing.Point(623, 351);
+            this.trainbtn.Name = "trainbtn";
+            this.trainbtn.Size = new System.Drawing.Size(75, 23);
+            this.trainbtn.TabIndex = 5;
+            this.trainbtn.Text = "训练图片";
+            this.trainbtn.UseVisualStyleBackColor = true;
+            this.trainbtn.Click += new System.EventHandler(this.trainbtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(588, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "名字";
+            // 
+            // labeltextBox
+            // 
+            this.labeltextBox.Location = new System.Drawing.Point(623, 301);
+            this.labeltextBox.Name = "labeltextBox";
+            this.labeltextBox.Size = new System.Drawing.Size(100, 21);
+            this.labeltextBox.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labeltextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trainbtn);
+            this.Controls.Add(this.savebtn);
+            this.Controls.Add(this.opencap);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button opencap;
+        private System.Windows.Forms.Button savebtn;
+        private System.Windows.Forms.Button trainbtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox labeltextBox;
+    }
+}
+
---
+++ facerecognize/facerecognize/Form1.cs
@@ -0,0 +1,128 @@
+﻿using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Face;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace facerecognize
+{
+    public partial class Form1 : Form
+    {
+        Capture _capture;
+        CascadeClassifier face_detect = new CascadeClassifier(@"haarcascade\haarcascade_frontalface_alt2.xml");//haarcascade_frontalface_alt2
+        Mat face_image = new Mat();
+        VectorOfInt image_lable = new VectorOfInt();
+        VectorOfMat image_data = new VectorOfMat();
+        string[] name;
+        FaceRecognizer face_recognize = new LBPHFaceRecognizer();
+        bool after_train = false;
+        bool catch_face = false;
+        ASCIIEncoding ascii = new ASCIIEncoding();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void frame(object sender,EventArgs e)
+        {
+            Mat scr = _capture.QueryFrame();
+            CvInvoke.Resize(scr, scr, new Size(320, 240));
+            Rectangle[] recs = face_detect.DetectMultiScale(scr);
+            foreach(Rectangle rec in recs)
+            {
+                CvInvoke.Rectangle(scr, rec, new MCvScalar(0, 0, 255));
+                imageBox2.Image = new Mat(scr, rec);
+                imageBox1.Image = scr;
+
+            }
+            if(catch_face)
+            {
+                if(labeltextBox.Text!=null)
+                {
+                    string labeltext = labeltextBox.Text;
+                    //MessageBox.Show(labeltext.ToString());
+                    //byte[] by = Encoding.GetEncoding("GB2312").GetBytes(labeltext);
+                    ////labeltext = ascii.GetString(by);
+                    //labeltext = Encoding.Unicode.GetString(by);
+
+                    //MessageBox.Show(labeltext);
+
+                    face_image = new Image<Gray, byte>(new Bitmap(imageBox2.Image.Bitmap)).Resize(120, 120, Inter.Area).Mat;
+                    face_image.Save("Image_data" + "\\" + labeltext + "_" + DateTime.Now.Year + DateTime.Now.Month + DateTime.Now.Day + ".jpg");
+                    MessageBox.Show("保存成功");
+                    catch_face = false;
+                }
+            }
+
+            if(after_train)
+            {
+                foreach(Rectangle rec in recs)
+                {
+                    string text = "";
+                    Mat scr_image = new Mat(scr, rec);
+                    scr_image = scr_image.ToImage<Gray, byte>().Resize(120, 120, Inter.Area).Mat;
+                    FaceRecognizer.PredictionResult result = face_recognize.Predict(scr_image);
+                    text = name[result.Label];
+                    if(result.Distance>3000)
+                    {
+                        text = "Miss";
+
+                    }
+                    CvInvoke.PutText(scr, text, rec.Location, FontFace.HersheyComplex, 1, new MCvScalar(0, 0, 255));
+                    imageBox1.Image = scr;
+
+                }
+            }
+        }
+
+        private void imageBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void opencap_Click(object sender, EventArgs e)
+        {
+            _capture = new Capture(0);
+            Application.Idle += frame;
+
+        }
+
+        private void savebtn_Click(object sender, EventArgs e)
+        {
+            catch_face = true;
+        }
+
+        private void trainbtn_Click(object sender, EventArgs e)
+        {
+            string[] path = Directory.GetFiles("Image_data");
+            Mat[] data = new Mat[path.Length];
+            int[] label = new int[path.Length];
+            name = new string[path.Length];
+            for(int i =0;i<path.Length;i++)
+            {
+                Image<Gray, byte> image = new Image<Gray, byte>(path[i]);
+                data[i] = image.Mat;
+                label[i] = i;
+                int a = path[i].LastIndexOf('\\');
+                int b = path[i].LastIndexOf('_');
+                name[i] = path[i].Substring(a+1, b - a - 1);
+
+            }
+            image_data.Push(data);
+            image_lable.Push(label);
+            face_recognize.Train(image_data, image_lable);
+            face_recognize.Save("face\\data.txt");
+            MessageBox.Show("训练成功");
+            after_train = true;
+            
+
+        }
+    }
+}
